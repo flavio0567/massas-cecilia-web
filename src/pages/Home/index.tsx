0ce7d586be0516c2daf6f3a7581d7aa985dd0709
Flavio Rocha <fmrocha@gmail.com>
2020-10-06 16:37:08 -0400

    feat. same treatment of family 3 as family 1 qtd

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -85,7 +85,7 @@ const Home: React.FC = () => {
       (prod: any) => prod.name.toLowerCase().indexOf(query) > -1,
     );
     setSelected(newSelection);
-  }, [products?.filter, query]);
+  }, [products?.filter, query, products]);
 
   const handleActivateProduct = useCallback(
     async (product: ProductFormData) => {
